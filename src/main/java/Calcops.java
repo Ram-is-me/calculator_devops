f27dd16b2dacb87a73a66a94790d406d21ff62ba
Ram-is-me <ram.s@iiitb.org>
2021-03-16 18:23:15 +0530

    Testing and Logging Complete

--- src/main/java/Calcops.java
+++ src/main/java/Calcops.java
@@ -1,10 +1,16 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calcops {
+    public static final Logger logger = LogManager.getLogger(Calcops.class);
     public double sqrt(double n)
     {
+        logger.info("Sqrt Calculation");
         return Math.sqrt(n);
     }
     public double factorial(double n)
     {
+        logger.info("Factorial Calculation");
         int a = (int) n;
         int result = 1;
         while(a>=1)
@@ -17,10 +23,12 @@ public class Calcops {
     }
     public double log(double n)
     {
+        logger.info("Natural Logarithm Calculation");
         return Math.log(n);
     }
     public double power(double m, double n)
     {
+        logger.info("Power Calculation");
         return Math.pow(m,n);
     }
 }
--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,14 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Calculator {
+    public static final Logger logger = LogManager.getLogger(Calculator.class);
+    public static Calcops c;
     public static void main(String[] args) {
         System.out.println("Welcome to the Scientific Calculator by S RAM (IMT2017521)!");
+        logger.info("Welcome Message sent");
         boolean loopcondition = true;
         while (loopcondition) {
             loopcondition = false;
@@ -12,24 +18,28 @@ public class Calculator {
             System.out.println("3. Natural Logarithm");
             System.out.println("4. Power Function");
             String choice;
-            Calcops c = new Calcops();
+            c = new Calcops();
             Scanner input =new Scanner(System.in);
             choice = input.nextLine();
             String temp;
             double answer = 0;
             if (choice.equals("1")) {
+                logger.info("Square Root Chosen");
                 System.out.println("Enter the number to find Square root:");
                 temp = input.nextLine();
                 answer = c.sqrt(Double.parseDouble(temp));
             } else if (choice.equals("2")) {
+                logger.info("Factorial Chosen");
                 System.out.println("Enter the number to find Factorial:");
                 temp = input.nextLine();
                 answer = c.factorial(Double.parseDouble(temp));
             } else if (choice.equals("3")) {
+                logger.info("Natural Logarithm Chosen");
                 System.out.println("Enter the number to find Natural Logarithm:");
                 temp = input.nextLine();
                 answer = c.log(Double.parseDouble(temp));
             } else if (choice.equals("4")) {
+                logger.info("Power Chosen");
                 System.out.println("Enter the number to find Power :");
                 System.out.println("Enter the base:");
                 temp = input.nextLine();
@@ -42,9 +52,11 @@ public class Calculator {
             System.out.println("Do you want to calculate again? [1:yes/0:no]");
             choice = input.nextLine();
             if (choice.equals("1")) {
+                logger.info("Calculate Again Chosen");
                 loopcondition = true;
             }
         }
+        logger.info("Exiting Program");
         System.out.println("Thank you!");
     }
 }
